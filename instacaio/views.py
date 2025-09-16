8c2cebb6905298ed99b6e0eed8c5c1aa0f18c8ed
caiohamamura <caiohamamura@gmail.com>
2025-09-16 02:39:41 -0300

    Ok

--- instacaio/views.py
+++ instacaio/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from instacaio.forms import PostForm
+from instacaio.models import Post
+
+
+def inicio(request):
+    posts = Post.objects.all()
+    return render(request, 'inicio.html', {
+        'posts': posts
+    })
 
 # Create your views here.
+def criar_post(request):
+    if (request.method == "POST"):
+        formulario = PostForm(request.POST, request.FILES)
+        if (formulario.is_valid):
+            formulario.save()
+            return redirect("inicio")
+    else:
+        formulario = PostForm()
+    return render(request, 'postar.html', {
+        "formulario": formulario
+    })
